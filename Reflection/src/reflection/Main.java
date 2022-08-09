5e5151fd46c21b05324e4bdfe35c439ef46d96af
MariaIvanova01 <mimi200801@abv.bg>
2022-08-09 15:55:07 +0300

    Implemented logic using reflection to print information

--- Reflection/src/reflection/Main.java
+++ Reflection/src/reflection/Main.java
@@ -1,8 +1,22 @@
 package reflection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+	    Class reflection = Reflection.class;
+
+        System.out.println(reflection);
+        Class superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+
+        Class[] interfaces = reflection.getInterfaces();
+        Arrays.stream(interfaces).forEach(System.out::println);
+
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+
     }
 }
