0f52a927948673c66e6f7a7e9458ddc7afd194d0
MariaIvanova01 <mimi200801@abv.bg>
2022-08-11 11:17:47 +0300

    Checkoing access modifiers to fields and methods

--- Reflection/src/reflection/Main.java
+++ Reflection/src/reflection/Main.java
@@ -1,6 +1,8 @@
 package reflection;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.Comparator;
 
@@ -9,20 +11,28 @@ public class Main {
     public static void main(String[] args) {
         Class<Reflection> reflection = Reflection.class;
 
+        Field[] fields = reflection.getDeclaredFields();
+
+        Arrays.stream(fields)
+                .filter(field -> !Modifier.isPrivate(field.getModifiers()))
+                .sorted(Comparator.comparing(Field::getName))
+                .forEach(field -> System.out.printf("%s must be private!%n",field.getName()));
+
         Method[] methods = reflection.getDeclaredMethods();
+
         Method[] getters = Arrays.stream(methods)
                 .filter(method -> method.getName().startsWith("get"))
                 .sorted(Comparator.comparing(Method::getName))
                 .toArray(Method[]::new);
 
-        Arrays.stream(getters)
-                .forEach(method -> System.out.printf("%s will return class %s%n", method.getName(), method.getReturnType().getName()));
+        Arrays.stream(getters).filter(getter -> !Modifier.isPublic(getter.getModifiers()))
+                .forEach(getter -> System.out.printf("%s have to be public!%n",getter.getName()));
 
         Method[] setters = Arrays.stream(methods).filter(method -> method.getName().startsWith("set"))
                 .sorted(Comparator.comparing(Method::getName))
                 .toArray(Method[]::new);
 
-        Arrays.stream(setters)
-                .forEach(method -> System.out.printf("%s and will set field of class %s%n", method.getName(), method.getParameterTypes()[0].getName()));
+        Arrays.stream(setters).filter(setter -> !Modifier.isPublic(setter.getModifiers()))
+                .forEach(setter -> System.out.printf("%s have to be private!%n",setter.getName()));
     }
 }
